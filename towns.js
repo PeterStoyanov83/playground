cef0eed831871f62388d5cd98bef67f55004c786
Emine Selimovska <emi956@abv.bg>
2024-05-28 13:38:19 +0300

    Add functionality

--- towns.js
+++ towns.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     $('#btnAdd').click(addTown);
-    $('#btnDelete').click(deleteTown);
-    $('#btnShuffle').click(shuffleTowns);
 });
 
 function addTown() {
@@ -9,42 +7,4 @@ function addTown() {
     $('#townNameForAdd').val('');
     $('#towns').append($('<option>').text(townName));
     showMessage(townName + " added.");
-}
-
-function deleteTown() {
-    let townName = $('#townName').val();
-    $('#townName').val('');
-    let removed = false;
-    for (let option of $('#towns option')) {
-        if (option.textContent == townName) {
-            removed = true;
-            option.remove();
-        }
-    }
-    if (removed)
-        showMessage(townName + " deleted.");
-    else
-        showMessage(townName + " not found.");
-}
-
-function shuffleTowns() {
-    let towns = $('#towns option').toArray();
-    $('#towns').empty();
-    shuffleArray(towns);
-    $('#towns').append(towns);
-    showMessage("Towns shuffled.");
-
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-}
-
-function showMessage(msg) {
-    $('#result').text(msg).css("display", "block");
-    setTimeout(function () {
-        $('#result').hide('blind', {}, 500);
-    }, 3000);
 }
